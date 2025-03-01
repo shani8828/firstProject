e3eab412d8fe46b78f6b0ddd0deaefd595068759
shani8828 <shani.maurya.iitkgp@gmail.com>
2025-03-01 22:17:16 +0530

    Squashed commit of the following:
    commit af698e78d961143a1e25ab3f22cb29b177157bbf
    Author: shani8828 <shani.maurya.iitkgp@gmail.com>
    Date:   Sat Mar 1 22:14:33 2025 +0530
    
        left updated
    
    commit 4a3b93c71187e69fc7c39fb0e278c0aa49c329b0
    Author: shani8828 <shani.maurya.iitkgp@gmail.com>
    Date:   Sat Mar 1 18:44:48 2025 +0530
    
        refined
    
    commit f99f11379476dd1c06402247ab3877b187f82c6a
    Author: shani8828 <shani.maurya.iitkgp@gmail.com>
    Date:   Sat Mar 1 18:41:02 2025 +0530
    
        sec-commit in feature-a
    
    commit 7bd200fe6856d60256d3325982793856a8e36f60
    Author: shani8828 <shani.maurya.iitkgp@gmail.com>
    Date:   Sat Mar 1 18:37:50 2025 +0530
    
        left added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import Left from './components/Left'
-import Right from './components/Right'
+import "./App.css";
+import Left from "./components/Left";
+import Right from "./components/Right";
 
 function App() {
-
   return (
     <>
-    <Left/>
-    <Right/>
+      <Left />
+      <Right />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Left = () => {
   return (
     <>
-      <div className="p-8 border border-sky-300 rounded-lg bg-rose-200 text-black">
+      <div className="p-8 border border-sky-300 rounded-lg bg-sky-700 text-sky-700 selection:bg-none selection:text-white hover:shadow-lg hover:shadow-sky-400 transition-shadow duration-300">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam sint
         voluptate consequatur provident fuga accusantium autem in tempora velit
         sequi doloremque, tenetur eius temporibus minima? Esse rerum nisi
