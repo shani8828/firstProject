7e5c95c73c595c6eec3da39fbf9ac0d29b4a6e1b
shani8828 <shani.maurya.iitkgp@gmail.com>
2025-03-01 18:38:34 +0530

    Merge branch 'feature-a'

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Left from './components/Left'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Heading1</h1>
+    <Left/>
     </>
   )
 }
---
+++ src/components/Left.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Left = () => {
+  return (
+    <>
+      <div className="p-8 border border-sky-300 rounded-lg bg-rose-200 text-black">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam sint
+        voluptate consequatur provident fuga accusantium autem in tempora velit
+        sequi doloremque, tenetur eius temporibus minima? Esse rerum nisi
+        doloremque pariatur eum minima recusandae dolorem cumque reprehenderit,
+        nobis quia repellat qui nostrum fugiat voluptates voluptatibus porro
+        doloribus tempore repudiandae natus sit?
+      </div>
+    </>
+  );
+};
+
+export default Left;
